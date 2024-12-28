368da46aa6c8cc2bae60915e085976a371cd9568
Swethag <124976509+Swethag14@users.noreply.github.com>
2024-12-28 21:44:15 +0530

    adding declaration.js in controlstatements.js

--- control statements.js
+++ control statements.js
@@ -98,3 +98,14 @@ c = 6 ;
 y = c++;
 console.log(y);
 
+//declaration.js
+a="KEC"
+console.log(a);
+var b="AIML"
+console.log(b);
+const c= "https://kongu.ac.in"
+console.log(c);
+let d= 10;
+console.log(d);
+c=5;
+console.log(c); 
